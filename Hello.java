cdf6189ca5e1e7424aa50daa4c199010dbc5b35e
mldn <1009000235@qq.com>
2018-01-30 23:13:33 +0800

    Add readme.txt

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello java");
 		System.out.println("Hello GIT");
+		System.out.println("Hello git");
+		System.out.println("Hello gits");
 	}
 }
