5f332df238bcdbcad9f8ee13265253ab2deaed4e
mldn <1009000235@qq.com>
2018-01-31 11:17:31 +0800

    Change Hello.java In Master Branch

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello java");
-		System.out.println("Hello GIT");
-		System.out.println("Hello git");
-		System.out.println("Hello gits");
+		Runtime run = Runtime.getRuntime();
+		run.gc();
 	}
 }
